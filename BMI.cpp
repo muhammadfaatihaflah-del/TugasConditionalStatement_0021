ab457026b121293640363d4b6f10841dbf13163b
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-03-09 21:14:28 +0700

    first commit

---
+++ BMI.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//prosedur memasukan data
+void inputData(float &berat, float &tinggi)
+{
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
